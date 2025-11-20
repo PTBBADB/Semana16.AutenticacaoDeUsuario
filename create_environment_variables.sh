555bd27610693d4995a8bfb35a144c1d849a92da
SorayaGomesS <sorayagomes62@gmail.com>
2025-11-20 01:54:37 -0300

    Add Mailgun API environment variables

--- create_environment_variables.sh
+++ create_environment_variables.sh
@@ -4,3 +4,7 @@ export MAIL_USERNAME=flaskaulas@zohomail.com
 export MAIL_PASSWORD=$1
 export FLASKY_ADMIN=fabio.teixeira@ifsp.edu.br
 export FLASKY_CONFIG=DevelopmentConfig
+
+export API_URL=https://api.mailgun.net/v3/sandboxc04db5dd6de5438093f5fa5c4591a9f1.mailgun.org/messages
+export API_KEY=58d874b4d52c69d4942c46d2470b2eb3-e80d8b76-bead1ea1
+export API_FROM="Mailgun Sandbox <postmaster@sandboxc04db5dd6de5438093f5fa5c4591a9f1.mailgun.org>"
